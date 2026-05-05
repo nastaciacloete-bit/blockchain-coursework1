d41a503181e7db16546d799691ed1c4d60d6d5bc
fc025091 <n.cloete@student.reading.ac.uk>
2026-05-05 13:46:26 +0000

    Blockchain.cs file for part 3, additions labeled in comments

---
+++ Blockchain.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BlockchainAssignment
+{
+    public class Blockchain
+    {
+        //list to store all  the blocks
+        public List<Block> blocks = new List<Block>();
+
+        //part 3: list to store pending transactions
+        public List<Transaction> transactionPool = new List<Transaction>();
+
+        //constructor to create genesis block
+        public Blockchain()
+        {
+            Block genesisBlock = new Block(); // calls empty constructor
+            blocks.Add(genesisBlock);
+        }
+
+        //gets the latest block in the chain
+        public Block GetLastBlock()
+        {
+            return blocks[blocks.Count - 1];
+        }
+
+        //adds a new block to the chain
+        public void AddBlock()
+        {
+            Block newBlock = new Block(GetLastBlock());
+            blocks.Add(newBlock);
+        }
+
+        //part 3: adds a transaction to the transaction pool
+        public void AddTransaction(Transaction transaction)
+        {
+            transactionPool.Add(transaction);
+        }
+
+        //reads a specific block by index
+        public string ReadBlock(int index)
+        {
+            if (index >= 0 && index < blocks.Count)
+            {
+                return blocks[index].ReadBlock();
+            }
+            else
+            {
+                return "Invalid block index.";
+            }
+        }
+
+        //reads all blocks
+        public string ReadAllBlocks()
+        {
+            StringBuilder output = new StringBuilder();
+
+            foreach (Block block in blocks)
+            {
+                output.AppendLine(block.ReadBlock());
+                output.AppendLine("---------------------------");
+            }
+
+            return output.ToString();
+        }
+
+        //part 3: reads all transactions waiting in the pool
+        public string ReadTransactionPool()
+        {
+            if (transactionPool.Count == 0)
+            {
+                return "Transaction pool is empty.";
+            }
+
+            StringBuilder output = new StringBuilder();
+
+            foreach (Transaction transaction in transactionPool)
+            {
+                output.AppendLine(transaction.ReadTransaction());
+                output.AppendLine("---------------------------");
+            }
+
+            return output.ToString();
+        }
+    }
+}
