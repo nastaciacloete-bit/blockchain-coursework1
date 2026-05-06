7683e335a01cbac2d0a14430043ce09d1f11c4b4
fc025091 <n.cloete@student.reading.ac.uk>
2026-05-06 15:23:36 +0000

    Transaction.cs file updated for part 5

---
+++ Transaction.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockchainAssignment
+{
+    public class Transaction
+    {
+        public string hash;
+        public string signature;
+        public string senderAddress;
+        public string recipientAddress;
+        public DateTime timestamp;
+        public double amount;
+        public double fee;
+
+        public Transaction(string senderAddress, string recipientAddress, double amount, double fee, string privateKey)
+        {
+            this.senderAddress = senderAddress;
+            this.recipientAddress = recipientAddress;
+            this.amount = amount;
+            this.fee = fee;
+            this.timestamp = DateTime.Now;
+
+            hash = CreateHash();
+
+            if (senderAddress == "Mine Rewards")
+            {
+                signature = "";
+            }
+            else
+            {
+                signature = Wallet.Wallet.CreateSignature(senderAddress, privateKey, hash);
+            }
+        }
+
+        public string CreateHash()
+        {
+            SHA256 hasher = SHA256Managed.Create();
+
+            string input = senderAddress + recipientAddress + timestamp.ToString() + amount.ToString() + fee.ToString();
+
+            byte[] hashByte = hasher.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            string hash = string.Empty;
+
+            foreach (byte x in hashByte)
+            {
+                hash += String.Format("{0:x2}", x);
+            }
+
+            return hash;
+        }
+
+        public string ReadTransaction()
+        {
+            return "Transaction Hash: " + hash +
+                   "\nSignature: " + signature +
+                   "\nSender: " + senderAddress +
+                   "\nRecipient: " + recipientAddress +
+                   "\nTimestamp: " + timestamp +
+                   "\nAmount: " + amount +
+                   "\nFee: " + fee;
+        }
+
+        //part 5:
+        public bool ValidateTransaction()
+        {
+            if (senderAddress == "Mine Rewards")
+            {
+                return true;
+            }
+
+            string recalculatedHash = CreateHash();
+
+            if (hash != recalculatedHash)
+            {
+                return false;
+            }
+
+            return Wallet.Wallet.ValidateSignature(senderAddress, hash, signature);
+        }
+
+        //part 5:
+        public bool ValidateHash()
+        {
+            return hash == CreateHash();
+        }
+    }
+}
