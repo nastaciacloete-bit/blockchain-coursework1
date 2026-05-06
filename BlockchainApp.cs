4f86f807eb05d283b1b5257948f21a4c96e67bda
fc025091 <n.cloete@student.reading.ac.uk>
2026-05-06 11:48:59 +0000

    fixes to miner key

--- BlockchainApp.cs
+++ BlockchainApp.cs
@@ -26,8 +26,14 @@ namespace BlockchainAssignment
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int index = int.Parse(textBox1.Text);
-            richTextBox1.Text = blockchain.ReadBlock(index);
+            if (int.TryParse(textBox1.Text, out int index))
+            {
+                richTextBox1.Text = blockchain.ReadBlock(index);
+            }
+            else
+            {
+                richTextBox1.Text = "Please enter a valid block index.";
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -108,7 +114,15 @@ namespace BlockchainAssignment
 
         private void button6_Click(object sender, EventArgs e)
         {
-            blockchain.AddBlock();
+            string minerAddress = textBox2.Text;
+
+            if (string.IsNullOrWhiteSpace(minerAddress))
+            {
+                richTextBox1.Text = "Please generate or enter a miner public key first.";
+                return;
+            }
+
+            blockchain.AddBlock(minerAddress);
             richTextBox1.Text = blockchain.ReadBlock(blockchain.blocks.Count - 1);
         }
 
